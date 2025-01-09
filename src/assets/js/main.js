bb7196b091cc785dd9743c2387d8747b0943b6a4
Ramin Khalouie <ra.khalouie@gmail.com>
2025-01-09 13:17:47 +0330

    [FEAT] : Complet settings section

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -69,3 +69,20 @@ switch (_date.getMonth()) {
 }
 dateTxt += " (" + _date.getDate() + ") " + _date.getFullYear();
 date.textContent = dateTxt;
+
+// Settings
+const settings = document.getElementById('settings');
+const imageParent = settings.querySelector('div');
+const images = imageParent.querySelectorAll('img');
+
+settings.addEventListener('click', () => {
+    imageParent.classList.toggle('hidden');
+    settings.classList.toggle('before:hidden');
+});
+images.forEach((el) => {
+    el.addEventListener('click', () => {
+        localStorage.setItem('bg', el.dataset.cs);
+        main.classList.remove(...['bg-img-1', 'bg-img-2', 'bg-img-3', 'bg-img-4', 'bg-img-5', 'bg-img-6', 'bg-img-7', 'bg-img-8', 'bg-img-9', 'bg-img-10', 'bg-img-11', 'bg-img-12', 'bg-img-13', 'bg-img-14', 'bg-img-15', 'bg-img-16', 'bg-img-17', 'bg-img-18', 'bg-img-19', 'bg-img-20', 'bg-img-21', 'bg-img-22', 'bg-img-23', 'bg-img-24', 'bg-img-25', 'bg-img-26', 'bg-img-27', 'bg-img-28', 'bg-img-29', 'bg-img-30'])
+        main.classList.add(el.dataset.cs);
+    })
+});
