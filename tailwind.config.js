ad83b0c88558f99192f5a932c3924d49d4c6d924
Ramin Khalouie <ra.khalouie@gmail.com>
2025-01-09 10:47:04 +0330

    [FEAT] : Add google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "Roboto": "Roboto",
+        "Rubik": "Rubik Vinyl"
+      }
+    },
   },
   plugins: [],
 }
